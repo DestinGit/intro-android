fa06aa1959db3a8446f7a64f5478211ecf2945e2
Destin <bak1tino@gmail.com>
2017-10-23 11:07:17 +0200

    first commit 2

---
+++ app/src/main/java/db/fr/intro/ActeurExo.java
@@ -0,0 +1,13 @@
+package db.fr.intro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActeurExo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acteur_exo);
+    }
+}
