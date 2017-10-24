361eefce39ce071425b6eac497f352f101b0b8d7
Destin <bak1tino@gmail.com>
2017-10-24 09:41:24 +0200

    mise en place du layout de ligne

--- app/src/main/java/db/fr/intro/ListeView1.java
+++ app/src/main/java/db/fr/intro/ListeView1.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,9 +16,15 @@ public class ListeView1 extends AppCompatActivity implements AdapterView.OnItemC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_view1);
 
+        String[] tVilles = {"Paris", "Marseille", "Lyon", "Nice"};
+
         liste = (ListView)findViewById(R.id.liste);
 
         liste.setOnItemClickListener(this);
+
+        ArrayAdapter<String> aaListe = new ArrayAdapter<String>(this, R.layout.ligne_avec_image, R.id.etiquette, tVilles);
+        liste.setAdapter(aaListe);
+
     }
 
     @Override
